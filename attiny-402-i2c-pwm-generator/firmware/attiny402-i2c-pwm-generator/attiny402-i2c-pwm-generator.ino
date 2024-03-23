e4f020fa11947a1907af26d457e94f506daf0daf
Cory Parsnipson <cparsnipson@gmail.com>
2024-03-23 01:52:58 -0700

    Implement EEPROM and I2C -> PWM control in attiny402 firmware.

--- attiny-402-i2c-pwm-generator/firmware/attiny402-i2c-pwm-generator/attiny402-i2c-pwm-generator.ino
+++ attiny-402-i2c-pwm-generator/firmware/attiny402-i2c-pwm-generator/attiny402-i2c-pwm-generator.ino
@@ -1,3 +1,4 @@
+#include <EEPROM.h>
 #include <Wire.h>
 
 // ------------------------------------------------------------------------
@@ -12,21 +13,41 @@
 // ------------------------------------------------------------------------
 #define PWM_OUTPUT_PIN      PIN_PA3
 #define PWM_MAX_PERIOD      0xFFFF
+#define PWM_DEFAULT_FREQ    1750
 
 // ------------------------------------------------------------------------
 // I2C defines
 // ------------------------------------------------------------------------
-#define NUM_REG    10
+#define NUM_REG    5
 
 // This is the I2C address to listen for commands on. Picked at random
 // to one that didn't seem very overloaded. Change this to any
 // convenient value if needed.
 #define I2C_ADDR   0x3F
 
+// I2C register indexes (for both system regs and eeprom)
+#define REG_ADDR_CTRL          0
+#define REG_ADDR_PWM_FREQ_LOW  1
+#define REG_ADDR_PWM_FREQ_HIGH 2
+#define REG_ADDR_PWM_DUTY_LOW  3
+#define REG_ADDR_PWM_DUTY_HIGH 4
+
+// REG_CTRL_DUTY_CYCLE_RESOLUTION
+// If the value at this bit is:
+//   0 = 16 bit res (0 to 65535)
+//   1 = 8 bit (0 to 255)
+#define REG_CTRL_DUTY_CYCLE_RESOLUTION 0
+
+// REG_CTRL_INVERT_MODE
+// This will invert the phase of the PWM. If this is set to 0, the
+// PWM signal will be high if the counter is < the CMP value. If set
+// to 1, the opposite will be true.
+#define REG_CTRL_INVERT_MODE 1
+
 // ------------------------------------------------------------------------
 // Serial defines
 // ------------------------------------------------------------------------
-#define SERIAL_ENABLED 1
+#define SERIAL_ENABLED 0
 #define SERIAL_MSG_BUFFER_SIZE 40
 #define SERIAL_BAUD_RATE  115200
 
@@ -50,8 +71,8 @@
 // ------------------------------------------------------------------------
 // Global variables
 // ------------------------------------------------------------------------
-volatile uint8_t device_registers[NUM_REG] = {0x00, 0x00, 0x00};
-const uint8_t write_mask[NUM_REG] = {0xFF, 0xFF};
+volatile uint8_t device_registers[NUM_REG] = {0x00, 0x00, 0x00, 0x00, 0x00};
+const uint8_t write_mask[NUM_REG] = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF};
 volatile uint8_t serial_write_pointer = 0;
 uint16_t period = 0xFFFF;
 
@@ -68,17 +89,21 @@ void setup() {
   // this needs to be set (even though it's implied to be DEFAULT) to have WO0
   // muxed to PA3. Probably something related to the mega tiny core lib changes.
   PORTMUX.CTRLC = PORTMUX_TCA00_DEFAULT_gc;
-  
+
   TCA0.SINGLE.CTRLB = (TCA_SINGLE_CMP0EN_bm | TCA_SINGLE_WGMODE_DSBOTTOM_gc); // set dual slope PWM mode, overflow on bottom
-  TCA0.SINGLE.PER = 0xFFFF; // count all the way up to 0xFFFF
+  TCA0.SINGLE.PER = PWM_MAX_PERIOD; // count all the way up to 0xFFFF
   TCA0.SINGLE.CMP0 = 0;
   TCA0.SINGLE.CTRLA = TCA_SINGLE_ENABLE_bm; // enable the timer with no prescaler
 
-  // TODO: source these values from I2C registers
-  setDutyCycle(0x7FFF, period);
-  setFrequency(1750, &period);
+  // restore configuration from non-volatile memory
+  readEEPROM((uint8_t*)device_registers, NUM_REG);
+  updateLocalVariables((uint8_t*)device_registers);
 
-  SERIAL_MSG("[PWM] init done (f: %d, d: 0x%X)", 1750, 0x7FFF);
+  SERIAL_MSG(
+    "[PWM] init (f: %ld, d: 0x%X)",
+    getFrequency((uint8_t*)device_registers),
+    getDutyCycle((uint8_t*)device_registers)
+  );
 }
 
 void loop() {}
@@ -131,7 +156,7 @@ void onI2CWrite(int num_bytes) {
   while (num_bytes > 0) {
     uint8_t orig_data = device_registers[serial_write_pointer] & ~write_mask[serial_write_pointer];
     device_registers[serial_write_pointer] = (Wire.read() & write_mask[serial_write_pointer]) | orig_data;
-    
+
     SERIAL_MSG(
       "[I2C] Wrote reg[%d] = 0x%X (mask: 0x%X)",
       serial_write_pointer,
@@ -142,7 +167,11 @@ void onI2CWrite(int num_bytes) {
     ++serial_write_pointer;
     serial_write_pointer = serial_write_pointer % NUM_REG;
     --num_bytes;
+
+    EEPROM.update(serial_write_pointer, device_registers[serial_write_pointer]);  
   }
+  
+  updateLocalVariables((uint8_t*)device_registers);
 }
 
 // Handler for I2C reads. When this chip receives a command like this:
@@ -165,4 +194,48 @@ void onI2CRead() {
   SERIAL_MSG("[I2C] Read reg[%d] = 0x%X (mask: 0x%X)", serial_write_pointer, read_val, write_mask[serial_write_pointer]);
   
   Wire.write(read_val);
+}
+
+void readEEPROM(uint8_t* registers, uint8_t num_regs) {
+  for (uint8_t addr = 0; addr < num_regs; ++addr) {
+    registers[addr] = EEPROM[addr];
+  }
+}
+
+uint16_t getDutyCycle(uint8_t* registers) {
+  uint16_t duty_cycle = registers[REG_ADDR_PWM_DUTY_HIGH];
+  duty_cycle = (duty_cycle << 8) | registers[REG_ADDR_PWM_DUTY_LOW];
+  return duty_cycle;
+}
+
+uint32_t getFrequency(uint8_t* registers) {
+  // frequency calculation works like this:
+  // lowest 14 bits are the base number, giving you a range of 0 to 16,384.
+  // The highest 2 bits are used as a x10 multiplier (so x1, x10, x100, x1000)
+  // letting you reach up to 16 MHz.
+  uint32_t freq = (registers[REG_ADDR_PWM_FREQ_HIGH] & 0x3F);
+  freq = (freq << 8) | registers[REG_ADDR_PWM_FREQ_LOW];
+
+  int freq_multi = registers[REG_ADDR_PWM_FREQ_HIGH] >> 6;
+  for (int i = 0; i < freq_multi; ++i) {
+    freq *= 10;
+  }
+  
+  if (freq == 0) {
+    SERIAL_MSG("[I2C][WARNING] 0 freq detected. Using default.");
+    freq = PWM_DEFAULT_FREQ;
+  }
+  
+  return freq;  
+}
+
+void updateLocalVariables(uint8_t* registers) {
+  setFrequency(getFrequency(registers), &period);
+  setDutyCycle(getDutyCycle(registers), period);
+
+  SERIAL_MSG(
+    "[PWM] set (f: %ld, d: 0x%X)",
+    getFrequency((uint8_t*)device_registers),
+    getDutyCycle((uint8_t*)device_registers)
+  );
 }
